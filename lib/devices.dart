5a7347961860805393812aaa5ba105cdbc62d209
moemam10 <emaamm10@gmail.com>
2025-11-06 17:34:24 +0200

    Update devices.dart

--- lib/devices.dart
+++ lib/devices.dart
@@ -1,10 +1,10 @@
 import 'package:hive/hive.dart';
 
-part 'devices.g.dart'; // هذا السطر مهم جدًا
+part 'devices.g.dart'; 
 
-@HiveType(typeId: 1) // typeId فريد لكل كائن
+@HiveType(typeId: 1) 
 class Device extends HiveObject {
-  // يجب أن يمتد من HiveObject
+ 
   @HiveField(0)
   final String id;
 
@@ -76,7 +76,7 @@ class Device extends HiveObject {
   }) : waitingList = waitingList ?? [];
 }
 
-// يجب أيضًا إنشاء Adapters للـ Enums
+
 @HiveType(typeId: 2)
 enum DeviceType {
   @HiveField(0)
